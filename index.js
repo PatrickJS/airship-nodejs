685a224639272d1c6a9b2eed3a11cf78d0c09493
Edward Zhang <ezhang90@gmail.com>
2018-02-10 21:58:33 -0800

    implement gatingInfo retrieval (every 5 minutes) and upload stats (uploads on hitting a batch size or by time since last upload)

--- index.js
+++ index.js
@@ -1,11 +1,130 @@
 import request from "superagent";
 
 export default class Airship {
-  constructor(options) {
+  constructor(options, cb) {
     this.apiKey = options.apiKey;
     this.environment = options.environment;
     this.timeout = options.timeout || 1000;
     this.transformer = options.transformer || (x => x);
+
+    // This is passed a reason.
+    this.gatingInfoErrorCb = options.gatingInfoErrorCb || (() => console.error('Airship: failed to retrieve gating info.'));
+    this.gatingInfo = null;
+    // Used to check whether we are already trying to get gatingInfo.
+    this.gatingInfoPromise = null;
+
+    var hardMaxGateStatsBatchSize = 500;
+    this.maxGateStatsBatchSize = options.maxGateStatsBatchSize != null // Allow 0 for no batching
+      ? Math.min(Math.max(options.maxGateStatsBatchSize, 0), hardMaxGateStatsBatchSize) : hardMaxGateStatsBatchSize;
+    this.gateStatsUploadBatchInterval = options.gateStatsUploadBatchInterval != null // Allow 0 for BatchInterval -> immediate
+      ? options.gateStatsUploadBatchInterval : 60; // in seconds
+    // This is the timer from setInterval for uploading stats. This timer is cleared and recreated
+    // when the batch size is reached, ensuring that stats uplead requests are always triggered
+    // within options.gateStatsUploadBatchInterval seconds of the event.
+    // More than one upload stats requests can simultaneously be in flight (unlike rules)
+    this.gateStatsUploadTimeout = null;
+    this.gateStatsBatch = [];
+  }
+
+  // If this is passed a callback as an argument, the arguments null, true will be passed on success,
+  // or an Error will be passed on failure.
+  // If this is not passed a callback as an argument, this will return a Promise that resolves when
+  // initialization is complete.
+  init(cb) {
+    if (this.gateStatsUploadBatchInterval > 0) {
+      this.gateStatsUploadTimeout = setInterval(this.triggerUploadStats, this.gateStatsUploadBatchInterval);
+    }
+
+    var getGatingInfoPromise = () => {
+      // TODO: get the url for this request
+      return request.get("gatingInfo-endpoint")
+        .set('Api-Key', this.apiKey)
+        .timeout(this.timeout)
+    }
+
+    // TODO: remove this fake one
+    var getFakeGatingInfoPromise = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          var gatingInfo = {
+            timestamp: Date.now()
+          }
+          console.log('Airship: retrieved gatingInfo', gatingInfo)
+          resolve(gatingInfo);
+        }, 500)
+      })
+    }
+
+    var maybeGetGatingInfoPromise = () => {
+      if (this.gatingInfoPromise) {
+        return;
+      }
+
+      this.gatingInfoPromise = getGatingInfoPromise().then(gatingInfo => {
+        this.gatingInfo = gatingInfo
+        this.gatingInfoPromise = null // this code should be in a .finally, but that may not be widely supported
+      }).catch(reason => {
+        this.gatingInfoPromise = null // this code should be in a .finally, but that may not be widely supported
+        this.gatingInfoErrorCb(reason)
+        throw reason
+      })
+      return this.gatingInfoPromise
+    }
+
+    var initialGatingInfoPromise = maybeGetGatingInfoPromise();
+    setInterval(maybeGetGatingInfoPromise, 5 * 60 * 1000);
+
+    if (cb) {
+      initialGatingInfoPromise
+        .then(() => cb(null, true))
+        .catch(() => cb(new Error("Airship: failed to initialize, will re-try in five (5) minutes.")))
+      return;
+    }
+
+    return initialGatingInfoPromise;
+  }
+
+  triggerUploadStats() {
+    var getUploadStatsPromise = () => {
+      var payload = this.gateStatsBatch
+      this.gateStatsBatch = []
+
+      // TODO: error handling on triggerUploadStats - do we try again?
+      // not right now, but we could add .then() referring to `payload` to put it back in the gateStatsBatch
+
+      // TODO: get the url for this request      
+      return request.post("upload-stats-endpoint")
+        .set('Api-Key', this.apiKey)
+        .timeout(this.timeout)
+        .send(payload)
+    }
+
+    // TODO: remove this fake one. The entire function body can be replaced with the new one after
+    var getFakeUploadStatsPromise = () => {
+      var payload = this.gateStatsBatch
+      this.gateStatsBatch = []
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Airship: uploaded stats ', payload)
+          resolve();
+        }, 500)
+      })
+    }
+
+    return getUploadStatsPromise();
+  }
+
+  _uploadStatsAsync(gateStats) {
+    this.gateStatsBatch.push(gateStats);
+    if (this.gateStatsUploadBatchInterval === 0 || this.gateStatsBatch.length >= this.maxGateStatsBatchSize) {
+      setImmediate(this.triggerUploadStats)
+    }
+
+    if (this.gateStatsUploadBatchInterval > 0) {
+      // recreate the setInterval Timeout
+      clearInterval(this.gateStatsUploadTimeout)
+      this.gateStatsUploadTimeout = setInterval(this.triggerUploadStats, this.gateStatsUploadBatchInterval);
+    }
   }
 
   _endpoint(objects, controlShortName) {
@@ -48,7 +167,31 @@ export default class Airship {
     });
   }
 
-  isEnabled(controlShortName, objects) {
+  isEnabled(controlShortName, object) {
+    // TODO: consider triggering another gatingInfo request if gatingInfo are not present, but we need to be
+    // careful with this.
+    if (this.gatingInfo == null) {
+      return false
+    }
+
+    // TODO: implement the line below
+    const gatesStats = Date.now() // TODO: remember to serialize in case objects change
+    this._uploadStatsAsync(gateStats)
+  }
+
+  getVariation(controlShortName, objects) {
+    // TODO: consider triggering another gatingInfo request if gatingInfo are not present, but we need to be
+    // careful with this.
+    if (this.gatingInfo == null) {
+      return null
+    }
+
+    // TODO: implement the line below
+    const gateStats = Date.now() // TODO: remember to serialize in case objects change
+    this._uploadStatsAsync(gateStats)
+  }
+
+  isEnabledAsync(controlShortName, objects) {
     return this._processEndpoint(
       controlShortName,
       objects,
@@ -56,7 +199,7 @@ export default class Airship {
     );
   }
 
-  getVariation(controlShortName, objects) {
+  getVariationAsync(controlShortName, objects) {
     return this._processEndpoint(
       controlShortName,
       objects,
